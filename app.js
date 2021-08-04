79b49d143fccdfcd7fc3b8dc4fc67303bf69b401
SIBIL JOE <sikurian@deloitte.ca>
2021-08-04 14:23:56 -0400

    App path adjusted

---
+++ app.js
@@ -0,0 +1,9 @@
+const express=require('express')
+const app=express();
+const port=process.env.PORT ||3000
+
+app.get('/',(req,res)=>{
+    res.send("Hello")
+})
+
+app.listen(port,()=>console.log('App is up on '+port))
